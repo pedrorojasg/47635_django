7c9df43e5d08003433b346127ab59b4c076257f8
Pedro Rojas Gavidia <pedro@example.com>
2023-01-02 19:48:04 -0400

    Agregados primeros models

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=64)
+    comision = models.IntegerField()
+
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    profesion = models.CharField(max_length=128)
+    bio = models.TextField()
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_entrega = models.DateTimeField()
+    esta_aprobado = models.BooleanField(default=False)
