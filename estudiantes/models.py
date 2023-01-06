57a9501b9ed8c649054ba8961433841b87ea8fcd
Pedro Rojas Gavidia <pedro@example.com>
2023-01-06 09:48:30 -0400

    Agregado campo a modelo Curso en after class

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=64)
     comision = models.IntegerField()
+    fecha_inicio = models.DateField(null=True)
 
     def __str__(self):
         return f"{self.nombre}, {self.comision}"
