b1bfe28807873cfaaae9f008172f723f4e8ac67a
Pedro Rojas Gavidia <pedro@example.com>
2023-01-23 19:23:07 -0400

    Agregado modelo y migracion de Avatar

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=64)
@@ -42,3 +44,13 @@ class Entregable(models.Model):
 
 class Instituto(models.Model):
     nombre = models.CharField(max_length=256)
+
+
+class Avatar(models.Model):
+    # Va a estar asociado con el User. Avatar es una tabla anexa de User
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    # upload_to es la subcarpeta dentro de la carpeta media
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
+
+    def __str__(self):
+        return f"Imagen de: {self.user}"
