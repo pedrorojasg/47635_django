763c95620c73071582986e68e5257608a5b932cd
Pedro Rojas Gavidia <pedro@example.com>
2023-01-23 18:59:26 -0400

    Agregada carpeta media

--- sistema_coder/settings.py
+++ sistema_coder/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+# Para guardado de imagenes
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
