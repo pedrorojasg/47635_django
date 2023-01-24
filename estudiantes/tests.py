f37f4e5724bffc959d010842cc610e53d4887f42
Pedro Rojas Gavidia <pedro@example.com>
2023-01-23 20:03:43 -0400

    EJemplo test unitario

--- estudiantes/tests.py
+++ estudiantes/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from estudiantes.models import Curso
+
+
+class CursoTests(TestCase):
+    """En esta clase van todas las pruebas del modelo Curso."""
+
+    def test_creacion_de_curso(self):
+        """
+        Prueba crear cursos con nombres cortos y largos
+        """
+        curso_nombre_valido = Curso.objects.create(
+            nombre="nombre corto", comision=101
+        )
+        # Compruebo que el curso fue creado
+        self.assertEqual(Curso.objects.all().count(), 1)
+        self.assertIsNotNone(curso_nombre_valido.id)
